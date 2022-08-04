5cb9fd5ab77171ff8eb0e765c16dbc4e58007c40
Matteo <matteo@mattiuzzi.com>
2022-08-04 20:42:56 +0200

    testpush

--- R/composeUrl.R
+++ R/composeUrl.R
@@ -42,6 +42,7 @@ composeUrl <- function(productType=c('FSC','GFSC', 'RLIE','SWS', 'WDS', 'PSA','P
   # sortParam: results are sorted according start date.
   # sortOrder: results are sorted in descending order (most recent first).
   
+  
   staticP <- paste('sortParam=startDate','sortOrder=descending','status=all',
                    'dataset=ESA-DATASET', sep='&')
 
